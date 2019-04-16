1fd27db5c77901ce67daddf2350f9aa3853bccdc
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-04-16 20:13:38 +0900

    shorten code using assertion macro

--- include/gazebo_continuous_track/gazebo_continuous_track.hpp
+++ include/gazebo_continuous_track/gazebo_continuous_track.hpp
@@ -19,72 +19,38 @@ public:
     std::cout << "[" << plugin_name << "]:"
               << " Start loading plugin" << std::endl;
 
-    //
-    if (!sdf->HasElement("link")) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [link] element for continuous track plugin" << std::endl;
-      return;
-    }
-    if (!sdf->HasElement("revolute_joint")) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [revolute_joint] element for continuous track plugin" << std::endl;
-      return;
-    }
-    if (!sdf->HasElement("gearbox_joint")) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [gearbox_joint] element for continuous track plugin" << std::endl;
-      return;
-    }
-    if (!sdf->HasElement("dpose")) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [dpose] element for continuous track plugin" << std::endl;
-      return;
-    }
-    if (!sdf->HasElement("count")) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [count] element for continuous track plugin" << std::endl;
-      return;
-    }
-
-    //
+    // get sdf description of the wheel link template
+    GZ_ASSERT(sdf->HasElement("link"), "No [link] element");
     const std::string link_name(sdf->Get< std::string >("link"));
     const physics::LinkPtr link(parent->GetLink(link_name));
-    if (!link) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [" << link_name << "] link" << std::endl;
-      return;
-    }
+    GZ_ASSERT(link, "Cannot find a link with the value of [link] element");
     const sdf::ElementPtr link_sdf(link->GetSDF());
 
-    //
+    // get sdf description of the wheel axle template
+    GZ_ASSERT(sdf->HasElement("revolute_joint"), "No [revolute_joint] element");
     const std::string rjoint_name(sdf->Get< std::string >("revolute_joint"));
     const physics::JointPtr rjoint(parent->GetJoint(rjoint_name));
-    if (!rjoint) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [" << rjoint_name << "] joint" << std::endl;
-      return;
-    }
+    GZ_ASSERT(rjoint, "Cannot find a joint with the value of [revolute_joint] element");
     const sdf::ElementPtr rjoint_sdf(rjoint->GetSDF());
 
-    //
+    // get sdf description of the belt constraint template
+    GZ_ASSERT(sdf->HasElement("gearbox_joint"), "No [gearbox_joint] element");
     const std::string gjoint_name(sdf->Get< std::string >("gearbox_joint"));
     const physics::JointPtr gjoint(parent->GetJoint(gjoint_name));
-    if (!gjoint) {
-      std::cerr << "[" << plugin_name << "]:"
-                << " No [" << gjoint_name << "] joint" << std::endl;
-      return;
-    }
+    GZ_ASSERT(gjoint, "Cannot find a joint with the value of [gearbox_joint] element");
     const sdf::ElementPtr gjoint_sdf(gjoint->GetSDF());
 
-    //
+    // read other properties
+    GZ_ASSERT(sdf->HasElement("dpose"), "No [dpose] element");
     const ignition::math::Pose3d dpose(sdf->Get< ignition::math::Pose3d >("dpose"));
+    GZ_ASSERT(sdf->HasElement("count"), "No [count] element");
     const int count(sdf->Get< int >("count"));
 
-    //
+    // populate wheels, axles, and belts using the templates
     ignition::math::Pose3d new_link_pose(
         link_sdf->GetElement("pose")->Get< ignition::math::Pose3d >());
     for (int i = 1; i <= count; ++i) {
-      //
+      // create new wheel
       const std::string new_link_name(link_name + boost::lexical_cast< std::string >(i));
       const sdf::ElementPtr new_link_sdf(link_sdf->Clone());
       new_link_sdf->GetAttribute("name")->Set(new_link_name);
@@ -96,7 +62,7 @@ public:
       std::cout << "[" << plugin_name << "]:"
                 << " Created " << new_link_name << std::endl;
 
-      //
+      // create new axle
       const std::string new_rjoint_name(rjoint_name + boost::lexical_cast< std::string >(i));
       const sdf::ElementPtr new_rjoint_sdf(rjoint_sdf->Clone());
       new_rjoint_sdf->GetAttribute("name")->Set(new_rjoint_name);
@@ -106,7 +72,7 @@ public:
       std::cout << "[" << plugin_name << "]:"
                 << " Created " << new_rjoint_name << std::endl;
 
-      //
+      // create new belt
       const std::string new_gjoint_name(gjoint_name + boost::lexical_cast< std::string >(i));
       const sdf::ElementPtr new_gjoint_sdf(gjoint_sdf->Clone());
       new_gjoint_sdf->GetAttribute("name")->Set(new_gjoint_name);
@@ -117,6 +83,7 @@ public:
                 << " Created " << new_gjoint_name << std::endl;
     }
 
+    // done!!
     std::cout << "[" << plugin_name << "]:"
               << " Loaded plugin" << std::endl;
   }
