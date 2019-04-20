f5db41a74cad7ecef0537b9a964b866f53b4b9f6
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-04-20 19:34:32 +0900

    reduce required elements

--- include/gazebo_continuous_track/gazebo_continuous_track.hpp
+++ include/gazebo_continuous_track/gazebo_continuous_track.hpp
@@ -13,74 +13,94 @@ namespace gazebo {
 
 class ContinuousTrack : public ModelPlugin {
 public:
-  void Load(physics::ModelPtr parent, sdf::ElementPtr sdf) {
-    const std::string plugin_name(sdf->GetAttribute("name")->GetAsString());
+  void Load(physics::ModelPtr parent_model, sdf::ElementPtr plugin_sdf) {
+    const std::string plugin_name(plugin_sdf->GetAttribute("name")->GetAsString());
 
     std::cout << "[" << plugin_name << "]:"
               << " Start loading plugin" << std::endl;
 
-    // get sdf description of the wheel link template
-    GZ_ASSERT(sdf->HasElement("link"), "No [link] element");
-    const std::string link_name(sdf->Get< std::string >("link"));
-    const physics::LinkPtr link(parent->GetLink(link_name));
-    GZ_ASSERT(link, "Cannot find a link with the value of [link] element");
-    const sdf::ElementPtr link_sdf(link->GetSDF());
+    // find the seed wheel link with the value of [seed_wheel] element
+    GZ_ASSERT(plugin_sdf->HasElement("seed_wheel"), "No [seed_wheel] element");
+    const physics::LinkPtr seed_wheel(
+        parent_model->GetLink(plugin_sdf->Get< std::string >("seed_wheel")));
+    GZ_ASSERT(seed_wheel, "Cannot find a link with the value of [seed_wheel] element");
+    const std::string seed_wheel_name(seed_wheel->GetName());
+    const sdf::ElementPtr seed_wheel_sdf(seed_wheel->GetSDF());
 
-    // get sdf description of the wheel axle template
-    GZ_ASSERT(sdf->HasElement("revolute_joint"), "No [revolute_joint] element");
-    const std::string rjoint_name(sdf->Get< std::string >("revolute_joint"));
-    const physics::JointPtr rjoint(parent->GetJoint(rjoint_name));
-    GZ_ASSERT(rjoint, "Cannot find a joint with the value of [revolute_joint] element");
-    const sdf::ElementPtr rjoint_sdf(rjoint->GetSDF());
+    // find the seed axle joint in parent joints of the seed wheel
+    std::string seed_axle_name;
+    sdf::ElementPtr seed_axle_sdf;
+    for (const physics::JointPtr &joint : seed_wheel->GetParentJoints()) {
+      // continuous joint is categorized to HINGE_JOINT in gazebo9 api
+      if (joint->GetType() & physics::Joint::HINGE_JOINT) {
+        seed_axle_name = joint->GetName();
+        seed_axle_sdf = joint->GetSDF();
+        break;
+      }
+    }
+    GZ_ASSERT(seed_axle_sdf,
+              "Cannot find a rotational joint in parent joints of the [seed_wheel] link");
 
-    // get sdf description of the belt constraint template
-    GZ_ASSERT(sdf->HasElement("gearbox_joint"), "No [gearbox_joint] element");
-    const std::string gjoint_name(sdf->Get< std::string >("gearbox_joint"));
-    const physics::JointPtr gjoint(parent->GetJoint(gjoint_name));
-    GZ_ASSERT(gjoint, "Cannot find a joint with the value of [gearbox_joint] element");
-    const sdf::ElementPtr gjoint_sdf(gjoint->GetSDF());
+    // find the seed belt joint in parent joints of the seed wheel
+    std::string seed_belt_name;
+    sdf::ElementPtr seed_belt_sdf;
+    for (const physics::JointPtr &joint : seed_wheel->GetParentJoints()) {
+      if (joint->GetType() & physics::Joint::GEARBOX_JOINT) {
+        seed_belt_name = joint->GetName();
+        seed_belt_sdf = joint->GetSDF();
+        break;
+      }
+    }
+    GZ_ASSERT(seed_belt_sdf,
+              "Cannot find a gearbox joint in parent joints of the [seed_wheel] link");
 
     // read other properties
-    GZ_ASSERT(sdf->HasElement("dpose"), "No [dpose] element");
-    const ignition::math::Pose3d dpose(sdf->Get< ignition::math::Pose3d >("dpose"));
-    GZ_ASSERT(sdf->HasElement("count"), "No [count] element");
-    const int count(sdf->Get< int >("count"));
+    GZ_ASSERT(plugin_sdf->HasElement("dpose"), "No [dpose] element");
+    const ignition::math::Pose3d dpose(plugin_sdf->Get< ignition::math::Pose3d >("dpose"));
+    GZ_ASSERT(plugin_sdf->HasElement("count"), "No [count] element");
+    const int count(plugin_sdf->Get< int >("count"));
 
-    // populate wheels, axles, and belts using the templates
-    ignition::math::Pose3d new_link_pose(
-        link_sdf->GetElement("pose")->Get< ignition::math::Pose3d >());
+    // populate wheels, axles, and belts using the seed entities
+    ignition::math::Pose3d new_wheel_pose(
+        seed_wheel_sdf->GetElement("pose")->Get< ignition::math::Pose3d >());
     for (int i = 1; i <= count; ++i) {
-      // create new wheel
-      const std::string new_link_name(link_name + boost::lexical_cast< std::string >(i));
-      const sdf::ElementPtr new_link_sdf(link_sdf->Clone());
-      new_link_sdf->GetAttribute("name")->Set(new_link_name);
-      new_link_pose += dpose;
-      new_link_sdf->GetElement("pose")->Set(new_link_pose);
-      const physics::LinkPtr new_link(parent->CreateLink(new_link_name));
-      new_link->Load(new_link_sdf);
-      new_link->Init();
-      std::cout << "[" << plugin_name << "]:"
-                << " Created " << new_link_name << std::endl;
+      // create a new wheel
+      const std::string new_wheel_name(seed_wheel_name + boost::lexical_cast< std::string >(i));
+      {
+        const sdf::ElementPtr sdf(seed_wheel_sdf->Clone());
+        sdf->GetAttribute("name")->Set(new_wheel_name);
+        new_wheel_pose += dpose;
+        sdf->GetElement("pose")->Set(new_wheel_pose);
+        const physics::LinkPtr wheel(parent_model->CreateLink(new_wheel_name));
+        wheel->Load(sdf);
+        wheel->Init();
+        std::cout << "[" << plugin_name << "]:"
+                  << " Created " << new_wheel_name << std::endl;
+      }
 
-      // create new axle
-      const std::string new_rjoint_name(rjoint_name + boost::lexical_cast< std::string >(i));
-      const sdf::ElementPtr new_rjoint_sdf(rjoint_sdf->Clone());
-      new_rjoint_sdf->GetAttribute("name")->Set(new_rjoint_name);
-      new_rjoint_sdf->GetElement("child")->Set(new_link_name);
-      const physics::JointPtr new_rjoint(parent->CreateJoint(new_rjoint_sdf));
-      new_rjoint->Init();
-      std::cout << "[" << plugin_name << "]:"
-                << " Created " << new_rjoint_name << std::endl;
+      // create a new axle
+      {
+        const std::string name(seed_axle_name + boost::lexical_cast< std::string >(i));
+        const sdf::ElementPtr sdf(seed_axle_sdf->Clone());
+        sdf->GetAttribute("name")->Set(name);
+        sdf->GetElement("child")->Set(new_wheel_name);
+        const physics::JointPtr axle(parent_model->CreateJoint(sdf));
+        axle->Init();
+        std::cout << "[" << plugin_name << "]:"
+                  << " Created " << name << std::endl;
+      }
 
-      // create new belt
-      const std::string new_gjoint_name(gjoint_name + boost::lexical_cast< std::string >(i));
-      const sdf::ElementPtr new_gjoint_sdf(gjoint_sdf->Clone());
-      new_gjoint_sdf->GetAttribute("name")->Set(new_gjoint_name);
-      new_gjoint_sdf->GetElement("child")->Set(new_link_name);
-      const physics::JointPtr new_gjoint(parent->CreateJoint(new_gjoint_sdf));
-      new_gjoint->Init();
-      std::cout << "[" << plugin_name << "]:"
-                << " Created " << new_gjoint_name << std::endl;
+      // create a new belt
+      {
+        const std::string name(seed_belt_name + boost::lexical_cast< std::string >(i));
+        const sdf::ElementPtr sdf(seed_belt_sdf->Clone());
+        sdf->GetAttribute("name")->Set(name);
+        sdf->GetElement("child")->Set(new_wheel_name);
+        const physics::JointPtr belt(parent_model->CreateJoint(sdf));
+        belt->Init();
+        std::cout << "[" << plugin_name << "]:"
+                  << " Created " << name << std::endl;
+      }
     }
 
     // done!!
