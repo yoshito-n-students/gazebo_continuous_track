a5e10b4831671ba4ae04939bf2f117e4dff32b9a
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2019-04-30 09:01:44 +0900

    refactor property loader

--- include/gazebo_continuous_track/gazebo_continuous_track_properties.hpp
+++ include/gazebo_continuous_track/gazebo_continuous_track_properties.hpp
@@ -14,7 +14,12 @@ class ContinuousTrackProperties {
 
 public:
   ContinuousTrackProperties(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
-    Load(_model, _sdf);
+    // assert the given sdf can be parsed as plugin property config
+    AssertPluginSDF(_sdf);
+
+    sprocket = LoadSprocket(_model, _sdf->GetElement("sprocket"));
+    trajectory = LoadTrajectory(_model, _sdf->GetElement("trajectory"));
+    pattern = LoadPattern(_model, _sdf->GetElement("pattern"));
   }
 
   virtual ~ContinuousTrackProperties() {}
@@ -53,45 +58,35 @@ private:
   // loading properties
   // ******************
 
-  // load all properties
-  void Load(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
-    // assert the given sdf can be parsed as plugin property config
-    const sdf::ElementPtr sdf(FormatAsPluginSDF(_sdf));
-
-    LoadSprocket(_model, sdf);
-    LoadTrajectory(_model, sdf);
-    LoadPattern(_model, sdf);
-  }
-
-  void LoadSprocket(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
+  static Sprocket LoadSprocket(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
     // format has been checked in Load(). no need to check if required elements exist.
 
-    // [sprocket]
-    const sdf::ElementPtr sprocket_elem(_sdf->GetElement("sprocket"));
+    Sprocket sprocket;
 
-    // []::[joint]
-    sprocket.joint = _model->GetJoint(sprocket_elem->GetElement("joint")->Get< std::string >());
+    // [joint]
+    sprocket.joint = _model->GetJoint(_sdf->GetElement("joint")->Get< std::string >());
     GZ_ASSERT(sprocket.joint,
               "Cannot find a joint with the value of [sprocket]::[joint] element in sdf");
     GZ_ASSERT(sprocket.joint->GetType() & physics::Joint::HINGE_JOINT,
               "[sprocket]::[joint] must be a rotatinal joint");
 
-    // []::[pitch_diameter]
-    sprocket.pitch_diameter = sprocket_elem->GetElement("pitch_diameter")->Get< double >();
+    // [pitch_diameter]
+    sprocket.pitch_diameter = _sdf->GetElement("pitch_diameter")->Get< double >();
+
+    return sprocket;
   }
 
-  void LoadTrajectory(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
+  static Trajectory LoadTrajectory(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
     // format has been checked in LoadProperties(). no need to check if required elements exist.
 
-    // [trajectory]
-    const sdf::ElementPtr traj_elem(_sdf->GetElement("trajectory"));
+    Trajectory trajectory;
 
-    // []::[segment] (multiple, +)
-    for (sdf::ElementPtr segment_elem = traj_elem->GetElement("segment"); segment_elem;
+    // [segment] (multiple, +)
+    for (sdf::ElementPtr segment_elem = _sdf->GetElement("segment"); segment_elem;
          segment_elem = segment_elem->GetNextElement("segment")) {
       Trajectory::Segment segment;
 
-      // []::[]::[joint]
+      // []::[joint]
       segment.joint = _model->GetJoint(segment_elem->GetElement("joint")->Get< std::string >());
       GZ_ASSERT(segment.joint, "Cannot find a joint with the value of "
                                "[trajectory]::[segment]::[joint] element in sdf");
@@ -99,33 +94,33 @@ private:
                     segment.joint->GetType() & physics::Joint::SLIDER_JOINT,
                 "[trajectory]::[segment]::[joint] must be a rotatinal or translational joint");
 
-      // []::[]::[end_position]
+      // []::[end_position]
       segment.end_position = segment_elem->GetElement("end_position")->Get< double >();
       GZ_ASSERT(segment.end_position > 0.,
                 "[trajectory]::[segment]::[end_position] must be positive real number");
 
       trajectory.segments.push_back(segment);
     }
+
+    return trajectory;
   }
 
-  void LoadPattern(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
+  static Pattern LoadPattern(const physics::ModelPtr &_model, const sdf::ElementPtr &_sdf) {
     // format has been checked in LoadProperties(). no need to check if required elements exist.
 
-    // [pattern]
-    const sdf::ElementPtr pattern_elem(_sdf->GetElement("pattern"));
+    Pattern pattern;
 
-    // []::[elements_per_round]
-    pattern.elements_per_round =
-        pattern_elem->GetElement("elements_per_round")->Get< std::size_t >();
+    // [elements_per_round]
+    pattern.elements_per_round = _sdf->GetElement("elements_per_round")->Get< std::size_t >();
     GZ_ASSERT(pattern.elements_per_round > 0,
               "[pattern]::[elements_per_round] must be positive intger");
 
-    // []::[element] (multiple, +)
-    for (sdf::ElementPtr element_elem = pattern_elem->GetElement("element"); element_elem;
+    // [element] (multiple, +)
+    for (sdf::ElementPtr element_elem = _sdf->GetElement("element"); element_elem;
          element_elem = element_elem->GetNextElement("element")) {
       Pattern::Element element;
 
-      // []::[]::[collision] (multiple, *)
+      // []::[collision] (multiple, *)
       if (element_elem->HasElement("collision")) {
         for (sdf::ElementPtr collision_elem = element_elem->GetElement("collision"); collision_elem;
              collision_elem = collision_elem->GetNextElement("collision")) {
@@ -133,7 +128,7 @@ private:
         }
       }
 
-      // []::[]::[visual] (multiple, *)
+      // []::[visual] (multiple, *)
       if (element_elem->HasElement("visual")) {
         for (sdf::ElementPtr visual_elem = element_elem->GetElement("visual"); visual_elem;
              visual_elem = visual_elem->GetNextElement("visual")) {
@@ -143,6 +138,8 @@ private:
 
       pattern.elements.push_back(element);
     }
+
+    return pattern;
   }
 
   // **************
@@ -161,11 +158,10 @@ private:
   // merge the plugin format sdf and the given sdf.
   // assert if the given sdf does not match the format
   // (ex. no required element, value type mismatch, ...).
-  static sdf::ElementPtr FormatAsPluginSDF(const sdf::ElementPtr &_src) {
-    static const sdf::ElementPtr seed(InitializedPluginSDF());
-    const sdf::ElementPtr dst(seed->Clone());
-    sdf::readString("<sdf version='" SDF_VERSION "'>" + _src->ToString("") + "</sdf>", dst);
-    return dst;
+  static void AssertPluginSDF(const sdf::ElementPtr &_sdf) {
+    static const sdf::ElementPtr fmt_seed(InitializedPluginSDF());
+    const sdf::ElementPtr fmt(fmt_seed->Clone());
+    sdf::readString("<sdf version='" SDF_VERSION "'>" + _sdf->ToString("") + "</sdf>", fmt);
   }
 };
 } // namespace gazebo
